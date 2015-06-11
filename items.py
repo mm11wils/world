c51697ce48baf1160279a63a35d851626be594b9
mm11wils <mm11wils@users.noreply.github.com>
2015-06-11 11:02:55 +0100

    Create items.py

---
+++ items.py
@@ -0,0 +1,48 @@
+import libtcodpy as ltc
+import math
+import textwrap
+import shelve
+
+class Item:
+    #an item that can be picked up and used.
+    def __init__(self, use_function=None):
+        self.use_function = use_function
+ 
+    def pick_up(self):
+        #add to the player's inventory and remove from the map
+        if len(inventory) >= 26:
+            message('Your inventory is full, cannot pick up ' + self.owner.name + '.', ltc.red)
+        else:
+            inventory.append(self.owner)
+            objects.remove(self.owner)
+            message('You picked up a ' + self.owner.name + '!', ltc.green)
+ 
+            #special case: automatically equip, if the corresponding equipment slot is unused
+            equipment = self.owner.equipment
+            if equipment and get_equipped_in_slot(equipment.slot) is None:
+                equipment.equip()
+ 
+    def drop(self):
+        #special case: if the object has the Equipment component, dequip it before dropping
+        if self.owner.equipment:
+            self.owner.equipment.dequip()
+ 
+        #add to the map and remove from the player's inventory. also, place it at the player's coordinates
+        objects.append(self.owner)
+        inventory.remove(self.owner)
+        self.owner.x = player.x
+        self.owner.y = player.y
+        message('You dropped a ' + self.owner.name + '.', ltc.yellow)
+ 
+    def use(self):
+        #special case: if the object has the Equipment component, the "use" action is to equip/dequip
+        if self.owner.equipment:
+            self.owner.equipment.toggle_equip()
+            return
+ 
+        #just call the "use_function" if it is defined
+        if self.use_function is None:
+            message('The ' + self.owner.name + ' cannot be used.')
+        else:
+            if self.use_function() != 'cancelled':
+                inventory.remove(self.owner)  #destroy after use, unless it was cancelled for some reason
